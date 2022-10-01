786d3da47ac82aad73b38f332743e83a20b2a0d1
Artur Baumeister <Artur.baumeister@gmail.com>
2022-10-01 16:18:11 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
-
+const express = require("express");
+const hbs = require("hbs");
 // require spotify-web-api-node package here:
+const SpotifyWebAPI = require("spotify-web-api-node");
 
-const app = express();
+const app = express(),
+  port = 3000;
 
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
 // setting the spotify-api goes here:
-
+const spotifyApi = new SpotifyWebAPI({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch(error => console.log("Something went wrong when retrieving an access token", error));
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.listen(port, () => console.log(`My Spotify project running on port ${port} 🎧 🥁 🎸 🔊`));
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
+console.log(spotifyApi);
